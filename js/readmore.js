509c84b8b04ce88f22c917c485cde4e88dc5f95d
Icellama <61001914+daniel-bergmann@users.noreply.github.com>
2020-07-16 09:33:36 +0000

    removed js

--- js/readmore.js
+++ js/readmore.js
@@ -107,20 +107,3 @@ function read5() {
   }
 }
 
-// Other JS
-
-// infinite scroll effect
-
-var elem = document.querySelector('.container');
-var infScroll = new InfiniteScroll( elem, {
-  // options
-  path: '.pagination__next',
-  append: '.post',
-  history: false,
-});
-
-// element argument can be a selector string
-//   for an individual element
-var infScroll = new InfiniteScroll( '.container', {
-  // options
-});
