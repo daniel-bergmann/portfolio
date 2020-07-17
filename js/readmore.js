ff82346148f8cbee31d0a170aa339182c2a03a9b
Icellama <61001914+daniel-bergmann@users.noreply.github.com>
2020-07-17 12:54:52 +0000

    added article

--- js/readmore.js
+++ js/readmore.js
@@ -121,3 +121,21 @@ function read5() {
   }
 }
 
+    //   Article 6
+
+    function read6() {
+      var dots = document.getElementById("dots6");
+      var moreText = document.getElementById("more6");
+      var btnText = document.getElementById("read6");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+      }
+    }
+
